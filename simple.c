efb75ff2a00e1467a8bcf0e4b526ec24e9b6ba3f
Ian Flury <ianflury123@gmai..com>
2022-12-02 00:37:58 -0800

    first attempt at setting up the struct

--- simple.c
+++ simple.c
@@ -4,10 +4,30 @@
 #include <linux/list.h>
 #include <linux/slab.h>
 
+
+struct person {
+       int month;
+       int day;
+       int year;
+       struct person *next;
+}
+
+struct person *ptr;
+
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
        printk(KERN_INFO "Loading Module\n");
+       
+       //create the first node in the list
+       ptr = (struct person*) malloc(5 * sizeof(struct person));
+
+       for (int i = 0; i < 4; i++) {
+              ptr->day = i;
+              ptr->month = i;
+              ptr->year = i;
+              ptr->next = (ptr+1);
+       }
 
        return 0;
 }
